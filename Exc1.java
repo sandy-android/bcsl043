967c69886e4526b90e24dca3609bdc8e6afc51da
sandy-android <svasava1051@gmail.com>
2025-05-17 12:33:13 +0000

    version1

---
+++ Exc1.java
@@ -0,0 +1,9 @@
+public class Exc1 {
+
+    public static void main(String args[]){
+        int height=10;
+        int width=15;
+        double area= height*width;
+        System.out.println("Area = "+ area);
+    }
+}
